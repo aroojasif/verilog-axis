0e652d0839002846dbc3a0dcd8b4016249ed5442
Arooj Asif <arooj.asif@desy.de>
2025-04-06 17:01:26 +0200

    customize file collection for synthesis and simulation

--- verilog_axis/module_verilog_axis.py
+++ verilog_axis/module_verilog_axis.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#
+
 # Copyright 2023 Nico De Simone.
 
 from pathlib import Path
@@ -7,34 +7,37 @@ from tsfpga.module import BaseModule
 from tsfpga.hdl_file import HdlFile
 
 class Module(BaseModule):
-    @property
-    def synthesis_folders(self):
-        """
-        Synthesis/implementation source code files will be gathered from these folders.
+    def __init__(self):
+        super().__init__(path=Path(__file__).parent.resolve(), library_name="verilog_axis")
 
-        Return:
-            list(pathlib.Path): Folder paths.
-        """
-        synthesis_folders = super().synthesis_folders
-        synthesis_folders.append(
-            self.path / "../rtl",
-        )
-        return synthesis_folders
+    def get_synthesis_files(self, **kwargs):
+        folders = [self.path.parent / "rtl"]
 
-    @property
-    def sim_folders(self):
-        """
-        Synthesis/implementation source code files will be gathered from these folders.
+        files = [
+            HdlFile(file_path)
+            for file_path in self._get_file_list(
+                    folders=folders, file_endings=(".vhd", ".vhdl", ".v")
+            )
+        ]
+        print("Synthesis files:", [str(f.path) for f in files])
+        return files
 
-        Return:
-            list(pathlib.Path): Folder paths.
-        """
-        sim_folders = super().sim_folders
-        sim_folders.append(
-            self.path / "../tb",
-        )
-        return sim_folders
-
+    def get_simulation_files(self, **kwargs):
+        folders = [self.path.parent / "rtl", self.path.parent / "test"]
+        return [
+            HdlFile(file_path)
+            for file_path in self._get_file_list(
+                folders=folders, file_endings=(".vhd", ".vhdl", ".v")
+            )
+        ]
 
 if __name__ == "__main__":
-    m = Module(path=Path(), library_name='verilog_axis')
+    m = Module()
+
+    print("Synthesis files:")
+    for f in m.get_synthesis_files():
+        print(" →", f.path)
+
+    print("Simulation files:")
+    for f in m.get_simulation_files():
+        print(" →", f.path)
